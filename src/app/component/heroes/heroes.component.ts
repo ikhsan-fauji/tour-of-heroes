9b9a83dc2b948b999f89f21bf79bad2788abbcbd
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-07-24 23:16:34 +0700

    move the heroes component to component folder

---
+++ src/app/component/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interface/hero';
+import { HeroService } from '../../service/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes?: Hero[];
+  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeros();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeros(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+}
