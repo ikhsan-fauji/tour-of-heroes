ca2e91f3f66a49f61e165e0364a3d5ce39efd18e
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-07-25 17:10:34 +0700

    setup the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
+import { HeroesComponent } from './component/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes/:id', component: HeroDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
