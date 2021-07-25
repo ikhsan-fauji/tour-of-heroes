80961297e4b861120ccf547c516ca3500ade6ab8
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-07-25 17:11:42 +0700

    move the hero detail component to component directory

--- src/app/component/hero-detail/hero-detail.component.ts
+++ src/app/component/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from 'src/app/service/hero.service';
 import { Hero } from '../../interface/hero';
 
 @Component({
@@ -10,9 +13,24 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
